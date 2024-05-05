3589fa6ad7d3425197d48722f0e7cc0059408251
lex55a <166326394+lex55a@users.noreply.github.com>
2024-05-05 20:51:42 +0300

    add code

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,12 +1,51 @@
 package tests;
 
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase {
 
-    @Test
-    public void test(){
+    @BeforeMethod
+    public void preCondition(){
+        //if SignOut is present ---->logout
+        if (app.getHelperUser().isLogged()) {
+            app.getHelperUser().logout();
+        }
+    }
 
+
+
+    @Test
+    public void loginSuccess(){
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("elena.krylataya@gmail.com", "Lenakira11$");
+        app.getHelperUser().submitLogin();
+
+        //  Assert
+//        Assert.assertEquals();
+//        Assert.assertNotEquals();
+//        Assert.assertTrue();
+//        Assert.assertFalse();
+
+        Assert.assertTrue(app.getHelperUser().isLogged());
+
+
+    }
+    @Test
+    public void loginSuccessModel(){
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("elena.krylataya@gmail.com","Lenakira11$");
+        app.getHelperUser().submitLogin();
+
+        //Assert
+//        Assert.assertEquals();
+//        Assert.assertNotEquals();
+//        Assert.assertTrue();
+//        Assert.assertFalse();
+
+        Assert.assertTrue(app.getHelperUser().isLogged());
 
     }
 
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -19,7 +19,7 @@ public class TestBase {
 
     @AfterSuite
     public void tearDown(){
-        app.stop();
+        // app.stop();
 
     }
 }
